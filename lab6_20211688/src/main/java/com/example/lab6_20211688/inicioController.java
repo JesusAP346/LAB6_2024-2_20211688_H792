338991829fdddc1b83616996891ebf1c0bfc5033
Jesus Alvarado <a2021188@pucp.edu.pe>
2024-10-03 20:37:20 -0500

    navbar en todas las vistas

--- lab6_20211688/src/main/java/com/example/lab6_20211688/inicioController.java
+++ lab6_20211688/src/main/java/com/example/lab6_20211688/inicioController.java
@@ -5,8 +5,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class inicioController {
-    @GetMapping("inicio")
+    @GetMapping("/inicio")
     public String irInicio(){
         return "inicio";
+    }
+    @GetMapping("directores")
+    public String directores(){
+        return "directores";
+    }
+    @GetMapping("contacto")
+    public String contacto(){
+        return "contacto";
+    }
+
+    @GetMapping("peliculas")
+    public String peliculas(){
+        return "peliculas";
     }
 }
